f80bf03da9dd3e6c52db63f0371c3efe6a98d682
Attila <attila@attila-sam.(none)>
2012-09-28 14:49:53 +0100

    Add du-sort-desc.sh

---
+++ du-sort-desc.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Displays the sizes of all subfolders in descending order. 
+# By default it lists only one subfolder level. Override with optional parameter $1, which is a number, meaning the level, e.g. '2'.
+
+
+
+if [ -n "$1" ]; then
+	depth=$1
+else
+	depth=1
+fi
+
+du -k -d ${depth} | sort -nr | awk '
+     BEGIN {
+        split("KB,MB,GB,TB", Units, ",");
+     }
+     {
+        u = 1;
+        while ($1 >= 1024) {
+           $1 = $1 / 1024;
+           u += 1
+        }
+        $1 = sprintf("%.1f %s", $1, Units[u]);
+        print $0;
+     }
+    '
+ 
