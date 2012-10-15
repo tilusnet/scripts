170924495bc94075778a1943e2ea02ab6bab83e9
Szász Attila <attila.szasz@tilusnet.com>
2012-10-15 15:24:41 +0100

    Added missing dbl-quotes in <find-in-tree.sh> parameters.

--- thq-find-in-tree.sh
+++ thq-find-in-tree.sh
@@ -5,10 +5,11 @@
 #   $3 = file pattern filter (optional)
 # e.g. $basename "." "nlp" "*.sh"
 
-# set -x
+set -x
 
-if [ -z $3 ]; then
+if [ -z "$3" ]; then
 	echo "Usage: `basename $0` <where> <what> <filepattern>"
+	echo " E.g.: `basename $0` \".\" \"nlp\" \"*.sh\""
 	exit 1
 fi
 
@@ -18,6 +19,6 @@ fi
 #	ffilt="-name $3"
 #fi
 
-find $1 -type f -name $3 -exec grep -HT --color "$2" {} \; 
+find "$1" -type f -name "$3" -exec grep -HT --color "$2" {} \; 
 # grep -i "$2" `find "$1" -type f -print` /dev/null # Doesn't work with spaces
 #find "$1" -type f -print0 | xargs -0 grep -iT --color "$2" /dev/null 
