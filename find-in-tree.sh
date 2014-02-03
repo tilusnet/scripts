b1b2f60c7dbe1e0a0aee35764af5b62f59ce366a
Szász Attila <attila.szasz@tilusnet.com>
2014-02-03 17:36:26 +0000

    Add more context to find-in-tree.

--- find-in-tree.sh
+++ find-in-tree.sh
@@ -19,6 +19,6 @@ fi
 #	ffilt="-name $3"
 #fi
 
-find "$1" -type f -name "$3" -exec grep -HTn --color "$2" {} \; 
+find "$1" -type f -name "$3" -exec grep -HTn --color -C 10 "$2" {} \; 
 # grep -i "$2" `find "$1" -type f -print` /dev/null # Doesn't work with spaces
 #find "$1" -type f -print0 | xargs -0 grep -iT --color "$2" /dev/null 
